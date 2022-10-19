ae5363316109b8dd1d7d7891b9fdd4b831352a36
David Sousa <davidnsousa@pm.me>
2022-10-19 10:35:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Archmage - Arch Linux Post Installation Setup
+
+Archmage is a simple post-install bash script to choose easily from a variety of packages for daily use from both the official Arch repositeries and AUR using yay. I wrote the script according to my software needs and XFCE preferences (theming, keyboard-shortcuts, etc) to help me replicate or reset my Arch system whenever I need. 
+
+Fork it and ghange it according to your needs.
+
+### Steps
+
+1. Install Arch Linux with __[archinstall](https://wiki.archlinux.org/title/Archinstall)__
+
+2. Clone and run archmage:
+
+```
+git clone https://github.com/davidnsousa/archmage
+cd archmage
+sh archmage.sh
---
+++ archmage.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+# PACKAGES TO CHOOSE FROM 
+
+PKGS=(
+
+    # TERMINAL UTILITIES
+
+    gnome-keyring "Gnome password storage" ON \
+    neofetch "Command-line system information tool" ON \
+    zsh "ZSH shell" ON \
+    zsh-completion "Tab completion for ZSH" ON \
+
+    # GENERAL UTILITIES
+
+    catfish "File search tool" ON \
+    bitwarden "Password manager" ON \
+
+    # DEVELOPMENT
+
+    base-devel "Development packages" ON \
+    geany "Text editor" ON \
+    git "Version control" ON \
+    octave "Scientific Programming Language" ON \
+    vscodium-bin "(AUR) Community-driven distribution of VSCode" ON \
+    rstudio-desktop-bin "(AUR) Development environment for R" ON \
+    arduino-ide-bin "(AUR) Arduino IDE" ON \
+
+    # WEB TOOLS
+
+    chromium "Open-source web browser" ON \
+    firefox "Mozilla web browser" ON \
+    librewolf-bin "(AUR) Customized Firefox for privacy and security" ON \
+    thunderbird "Mozilla e-mail client" ON \
+    transmission-gtk "BitTorrent client" ON \
+
+    # CLOUD UTILITIES
+
+    megasync-bin "(AUR) MEGA Desktop App" ON \
+    thunar-megasync-bin "(AUR) MEGA thunar utility" ON \
+
+    # COMMUNICATIONS
+
+    zoom "(AUR) Video Communications" ON \
+    telegram-desktop-bin "(AUR) Instant messaging service" ON \
+    slack-desktop "(AUR) Messaging program for office" ON \
+    signal-desktop-beta-bin "(AUR) Secure messaging service" ON \
+
+    # MEDIA
+
+    vlc "Media player" ON \
+
+    # GRAPHICS AND DESIGN
+
+    gcolor2 "Gnome color picker" ON \
+    gimp "Image editor" ON \
+    inkscape "Vector image editor" ON \
+
+    # PRODUCTIVITY
+
+    galculator "Gnome calculator" ON \
+    mousepad "Text editor" ON \
+    xpdf "PDF reader for X" ON \
+    onlyoffice-bin "(AUR) Office suite" ON \
+    zotero-bin "(AUR) Reference manager" ON \
+
+    # VIRTUALIZATION
+
+    virtualbox "Hypervisor for x86 virtualization" ON \
+    virtualbox-host-modules-arch "Virtualbox host kernel modules for Arch Kernel" ON \
+    virtualbox-host-modules-dkms "Virtualbox host kernel modules for other Kernels" OFF \
+
+    # PRIVACY AND SECURITY TOOLS
+
+    mullvad-vpn-bin "(AUR) Mullvad VPN service" ON \
+    protonmail-bridge-bin "(AUR) Protonmail bidge for e-mail clients" ON \
+
+    # OTHER
+
+    stellarium-bin "(AUR) Astronomy software" ON 
+)
+
+COSMETICS=(
+    rofi "Application launcher" ON \
+    arc-solid-gtk-theme "Arc GTK theme" ON \
+    arc-icon-theme "Arc icon theme" ON \
+    xfce4-panel-profiles "(AUR) Panel profiles for XFCE" ON
+)
+
+# INSTALL YAY
+
+if yay --version; then 
+    echo
+else 
+    if whiptail --yesno "yay is not installed. To use this script you need an AUR helper that extends to pacman. Do you whish to install yay?" 10 50; then
+        cd ${HOME}
+        echo "INSTALL YAY"
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+        yay --save --nocleanmenu --nodiffmenu
+        cd ${HOME}
+        rm -r yay
+    else
+        echo "OK!"
+    fi
+fi 
+
+# SELECT PACKAGES
+
+SELECTION=( $(whiptail --title "Install software" --separate-output --checklist "Select packages:" 22 100 14 "${PKGS[@]}" 3>&1 1>&2 2>&3) )
+
+# INSTALL PACKAGES
+
+if whiptail --yesno "Continue installation?" 10 50; then
+    for PKG in ${SELECTION[@]}; do
+        yay -S $PKG
+    done
+else
+    echo "OK!"
+fi
+
+# COSTUMIZATION
+
+if whiptail --yesno "Costumize XFCE?" 10 50; then
+    SELECTIONCOS=( $(whiptail --title "Theming & utilities" --separate-output --checklist "Select packages (official):" 22 100 14 "${COSMETICS[@]}" 3>&1 1>&2 2>&3) )
+    for PKG in ${SELECTIONCOS[@]}; do
+        yay -S $PKG
+    done 
+    cp -r config/xfce4/xfconf/xfce-perchannel-xml/. ${HOME}/.config/xfce4/xfconf/xfce-perchannel-xml/
+else
+    echo "OK!"
+fi
+
+echo
+echo "Done! Reboot if necessary."
+echo

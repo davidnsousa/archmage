aa8e8c24ad663c126279954b6ab19a0e840743c1
David Sousa <davidnsousa@pm.me>
2022-10-23 10:48:47 +0100

    minor

--- archmage.sh
+++ archmage.sh
@@ -186,7 +186,7 @@ if whiptail --yesno "$message2" 10 70; then
     xfconf-query -c xfce4-desktop -n -t 'bool' -p /desktop-menu/show -s false
     # no save on exit
     xfconf-query -c xfce4-session -n -t 'bool' -p /general/SaveOnExit -s false 
-    # window cycle preview false
+    # no window cycle preview
     xfconf-query -c xfwm4 -p /general/cycle_preview -s false  
     
     # LIGHTDM SETTINGS

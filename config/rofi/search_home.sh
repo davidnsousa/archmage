6cf9e39974f9f244c25cdbc9fc9808f479bd1310
David Sousa <davidnsousa@protonmail.com>
2022-11-04 10:22:54 +0000

    changed rofi menus

--- config/rofi/search_home.sh
+++ config/rofi/search_home.sh
@@ -2,6 +2,6 @@
 
 options="$(find /home/david -print)"
 
-chosen="$(echo -e "$options" | rofi -dmenu )"
+chosen="$(echo -e "$options" | rofi -dmenu -config ~/.config/rofi/config_search_home.rasi)"
 
 xdg-open "$chosen"
